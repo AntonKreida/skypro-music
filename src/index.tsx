73e7665e2527369818c9c784e70ca688d4dc48e7
Kreida Anton <kreida.anton@yandex.ru>
2023-08-13 14:57:14 +0500

    homewokr-2 | add craco, del husky, add global style, paths, fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { GlobalStyle } from '@style/';
 import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>
 );
---
+++ src/style/global/global.tsx
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        width: 100vw;
+        height: 100vh;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    h1, h2, h3, h4 {
+        margin: 0;
+    }
+
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+
+    a {
+        display: block;
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    img {
+        display: block;
+        height: auto;
+        max-width: 100%;
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    #root {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    } 
+`;
