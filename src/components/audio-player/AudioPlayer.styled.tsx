67612c6587ef4042c34ecd575fef551c0b84d617
Kreida Anton <kreida.anton@yandex.ru>
2023-08-15 03:35:04 +0500

    homework-2 | add audioplayer

--- src/components/audio-player/AudioPlayer.styled.tsx
+++ src/components/audio-player/AudioPlayer.styled.tsx
@@ -3,9 +3,88 @@ import styled from 'styled-components';
 
 export const AudioPlayerWrapper = styled.div`
     position: fixed;
-    width: 100vw;
-    height: 73px;
-    background: #1c1c1c7f;
     left: 0;
     bottom: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100vw;
+    height: 73px;
+    padding: 10px 36px 5px 36px;
+    background: #1c1c1c7f;
+`;
+
+export const AudioPlayerProgress = styled.div`
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 5px;
+    background: #2E2E2E;
+`;
+
+export const AudioPlayerControllerWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 30px;
+`;
+
+export const AudioPlayerPanel = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 33px;
+`;
+
+export const AudioPlayerButton = styled.button`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: fit-content;
+    height: fit-content;
+    border: none;
+    background: none;
+
+    & > svg {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 20px;
+        height: 20px;
+    }
+
+    & > .volume {
+        stroke: #fff;
+        fill: #fff;
+    }
+`;
+
+export const AudioPlayerInfoWrapper = styled.div`
+    display: flex;
+    gap: 5px;
+`;
+
+export const AudioPlayerInfoIconWrapper = styled.div`
+    width: 51px;
+    height: 51px;
+    background: #2E2E2E;
+`;
+
+export const AudioPlayerInfoTextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 3px;
+`;
+
+export const AudioPlayerInfoText = styled.div`
+    height: 15px;
+    width: 50px;
+    background: #2E2E2E;
+`;
+
+export const AudioPlayerInputRange = styled.input`
+    accent-color: #fff;
 `;
--- src/components/audio-player/AudioPlayer.tsx
+++ src/components/audio-player/AudioPlayer.tsx
@@ -2,5 +2,67 @@ import * as Styled from './AudioPlayer.styled';
 
 
 export const AudioPlayer = () => (
-  <Styled.AudioPlayerWrapper />
+  <Styled.AudioPlayerWrapper>
+
+    <Styled.AudioPlayerProgress />
+
+    <Styled.AudioPlayerControllerWrapper>
+      <Styled.AudioPlayerPanel>
+
+        <Styled.AudioPlayerButton>
+          <svg>
+            <use href={ `${process.env.PUBLIC_URL}/assets/icon/icons.svg#prev` } />
+          </svg>
+        </Styled.AudioPlayerButton>
+        <Styled.AudioPlayerButton>
+          <svg>
+            <use href={ `${process.env.PUBLIC_URL}/assets/icon/icons.svg#play` } />
+          </svg>
+        </Styled.AudioPlayerButton>
+        <Styled.AudioPlayerButton>
+          <svg>
+            <use href={ `${process.env.PUBLIC_URL}/assets/icon/icons.svg#next` } />
+          </svg>
+        </Styled.AudioPlayerButton>
+
+        <Styled.AudioPlayerButton>
+          <svg>
+            <use href={ `${process.env.PUBLIC_URL}/assets/icon/icons.svg#loop` } />
+          </svg>
+        </Styled.AudioPlayerButton>
+        <Styled.AudioPlayerButton>
+          <svg>
+            <use href={ `${process.env.PUBLIC_URL}/assets/icon/icons.svg#case` } />
+          </svg>
+        </Styled.AudioPlayerButton>
+
+        <Styled.AudioPlayerInfoWrapper>
+          <Styled.AudioPlayerInfoIconWrapper />
+          <Styled.AudioPlayerInfoTextWrapper>
+            <Styled.AudioPlayerInfoText />
+            <Styled.AudioPlayerInfoText />
+          </Styled.AudioPlayerInfoTextWrapper>
+        </Styled.AudioPlayerInfoWrapper>
+
+        <Styled.AudioPlayerButton>
+          <svg>
+            <use href={ `${process.env.PUBLIC_URL}/assets/icon/icons.svg#like` } />
+          </svg>
+        </Styled.AudioPlayerButton>
+
+      </Styled.AudioPlayerPanel>
+    </Styled.AudioPlayerControllerWrapper>
+
+    <Styled.AudioPlayerControllerWrapper>
+
+      <Styled.AudioPlayerPanel>
+        <Styled.AudioPlayerButton>
+          <svg className="volume">
+            <use href={ `${process.env.PUBLIC_URL}/assets/icon/icons.svg#volume` } />
+          </svg>
+        </Styled.AudioPlayerButton>
+        <Styled.AudioPlayerInputRange type="range" />
+      </Styled.AudioPlayerPanel>
+    </Styled.AudioPlayerControllerWrapper>
+  </Styled.AudioPlayerWrapper>
 );
